1bdc9d17e49be14b21be77eef366f59f6aa814ae
fleash23061986 <35706842+fleash23061986@users.noreply.github.com>
2018-01-30 22:34:50 +0300

    FirstProgram
    Add My first program

---
+++ MyFirstProgram.java
@@ -0,0 +1,7 @@
+public class MyFirstProgram{
+
+	public static void main(String[] args) {
+		System.out.println("It's my first repository from github - https://github.com/fleash23061986/FirstDevel.git");
+	}
+
+}
