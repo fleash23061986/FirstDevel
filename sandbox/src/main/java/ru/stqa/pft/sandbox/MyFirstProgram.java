b8b0ee2db33b98c0b0fe3ee99f631763d6b65b3c
fleash23061986 <35706842+fleash23061986@users.noreply.github.com>
2018-01-31 01:13:08 +0300

    kj b

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,10 +5,7 @@ public class MyFirstProgram{
 
 	public static void main(String[] args) {
 		System.out.println("It's my first repository from github - https://github.com/fleash23061986/FirstDevel.git");
-		System.out.println(2 + 2);
-		System.out.println(2 * 2);
-		System.out.println(2 / 2);
-		System.out.println(1.0 / 2);
+
 	}
 
 }
